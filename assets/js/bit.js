0031903c139064c41e8da8bbd0e1ce8181335813
Joowoonb <53214077+vickp@users.noreply.github.com>
2025-05-30 22:28:08 +0900

    update: bit style

--- assets/js/bit.js
+++ assets/js/bit.js
@@ -14,7 +14,7 @@ function calculateBitInfo() {
     
     // 결과 표시
     let resultText = `${bits}비트로는 ${infoCount.toLocaleString()}개의 정보를 표현할 수 있습니다.<br>`;
-    resultText += `이는 2<sup>${bits}</sup> = ${infoCount.toLocaleString()}입니다.`;
+    resultText += `이는 2^${bits} = ${infoCount.toLocaleString()}입니다.`;
     
     bitResult.innerHTML = resultText;
 } 
